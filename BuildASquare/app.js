6fda3cff830dcfb12728ab5b2e02594838602b11
gary909 <25279957+gary909@users.noreply.github.com>
2023-07-18 13:09:00 +0200

    code works

--- BuildASquare/app.js
+++ BuildASquare/app.js
@@ -1,5 +1,16 @@
 function generateShape(integer){
- return "hi"
+ let myStr = '';
+ let myArr = [];
+ 
+ for(let i = 0; i < integer; i++){
+     myStr = myStr + "+"
+    }
+
+ for(let i = 0; i < integer; i++){
+    myArr.push(myStr)
+   }
+    
+ return myArr.join('\n');
 }
 
 console.log(generateShape(8)); // 
