a5ebc6ea8f4084d4f46c79a8b3c4a6b9e31c4b00
gary909 <25279957+gary909@users.noreply.github.com>
2023-07-19 12:50:14 +0200

    kata switcheroo

---
+++ Switcheroo/app.js
@@ -0,0 +1,7 @@
+function switcheroo(x){
+
+}
+
+console.log(switcheroo('abc')); // 'bac'
+console.log(switcheroo('aaabcccbaaa')); // 'bbbacccabbb'
+console.log(switcheroo('ccccc')); // 'ccccc'
---
+++ Switcheroo/readme.md
@@ -0,0 +1,7 @@
+# Kata-Switcheroo
+
+![screen image](pic.png)
+
+![code image](code.png)
+
+![console image](con.png)
