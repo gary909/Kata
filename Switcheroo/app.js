c1210d6ab4a58618bb8a0c975c892653ad42cd5a
gary909 <25279957+gary909@users.noreply.github.com>
2023-07-19 12:56:07 +0200

    code working

--- Switcheroo/app.js
+++ Switcheroo/app.js
@@ -1,5 +1,15 @@
 function switcheroo(x){
-
+    let myStr = '';
+    for(let i = 0; i < x.length; i++){
+        if(x[i] == 'a'){
+            myStr = myStr + 'b';
+        } else if (x[i] == 'b'){
+            myStr = myStr + 'a';
+        } else if (x[i] == 'c'){
+            myStr = myStr + 'c';
+        }
+    }
+    return myStr
 }
 
 console.log(switcheroo('abc')); // 'bac'
