e1a9656d8072ac27d433b3993a292bf912e78170
gary909 <25279957+gary909@users.noreply.github.com>
2023-07-20 13:09:03 +0200

    code working now

--- CouponCode/app.js
+++ CouponCode/app.js
@@ -1,27 +1,29 @@
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
     
-    const d1 = new Date(currentDate);
-    const d2 = new Date(expirationDate);
-
-    // if(d1 < d2){
-    //     return "Date 1 is before Date 2";
-    // }
+    const currentDateD1 = new Date(currentDate);
+    const expirationDateD2 = new Date(expirationDate);
 
       if(enteredCode != correctCode){
         return false;
-      } else if (d1 < d2){
+      } else if (currentDateD1 > expirationDateD2){
+        return false;
+      } else {
         return true;
       }
-
-    //   return myArr;
-
-    // return myArr[0];
-
-    // const d1 = Date.parse(expirationDate);
-    // const d2 = Date.parse(currentDate);
-
-    // return d1
 }
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    let expires = new Date(expirationDate);
+    let current = new Date(currentDate);
+    
+      if (enteredCode !== correctCode){
+        return false;
+      }else if(current > expires){
+        return false;
+      }else{
+        return true;
+      }
+    }
 
 console.log(checkCoupon('123','123','September 5, 2014','October 1, 2014')); // true
 console.log(checkCoupon('123a','123','September 5, 2014','October 1, 2014')); // false
