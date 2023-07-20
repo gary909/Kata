382c5bbb12b5ddd5ece6d1b93fe0ce5454e231d3
gary909 <25279957+gary909@users.noreply.github.com>
2023-07-20 12:29:28 +0200

    code

---
+++ CouponCode/app.js
@@ -0,0 +1,7 @@
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    
+  
+}
+
+console.log(checkCoupon('123','123','September 5, 2014','October 1, 2014')); // true
+console.log(checkCoupon('123a','123','September 5, 2014','October 1, 2014')); // false
---
+++ CouponCode/readme.md
@@ -0,0 +1,6 @@
+
+![screen image](pic.png)
+
+![code image](code.png)
+
+![con image](con.png)
