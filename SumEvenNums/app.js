09049bde9559f1a04d0c888273d41e1c998e6ae0
gary909 <25279957+gary909@users.noreply.github.com>
2023-07-27 12:43:39 +0200

    kata

---
+++ SumEvenNums/app.js
@@ -0,0 +1,5 @@
+function sumEvenNumbers(input) {
+  // [...]
+}
+
+console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 30
---
+++ SumEvenNums/readme.md
@@ -0,0 +1,7 @@
+# Kata Sum even numbers
+
+![screen image](pic.png)
+
+![code image](code.png)
+
+![console image](con.png)
