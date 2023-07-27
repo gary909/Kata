8c62b5d5160119a80726963532796ee6f378f735
gary909 <25279957+gary909@users.noreply.github.com>
2023-07-27 12:49:26 +0200

    code working, readme images

--- SumEvenNums/app.js
+++ SumEvenNums/app.js
@@ -1,5 +1,11 @@
 function sumEvenNumbers(input) {
-  // [...]
+  let myNum = 0;
+  for(let i = 0; i < input.length; i++){
+        if(input[i] % 2 === 0) {
+            myNum = myNum + input[i]
+        }
+  }
+    return myNum;
 }
 
 console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 30
