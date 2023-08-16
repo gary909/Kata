a6b678132f7a3bbe66bbbbf633b9a16b0a6347a3
gary909 <25279957+gary909@users.noreply.github.com>
2023-08-16 13:25:21 +0200

    code working

--- MostDigits/app.js
+++ MostDigits/app.js
@@ -1,7 +1,8 @@
 function findLongest(array){
-    // code here
+    return array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a))
 }
 
 console.log(findLongest([1, 10, 100])); // 100
 console.log(findLongest([9000, 8, 800])); // 9000
 console.log(findLongest([8, 900, 500])); // 900
+console.log(findLongest([100980, 252350, 8])); // 900
