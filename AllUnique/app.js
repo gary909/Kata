0aaec657475d330197cf4fe85cdd4bc4023f869b
gary909 <25279957+gary909@users.noreply.github.com>
2023-08-18 17:43:13 +0200

    code working

--- AllUnique/app.js
+++ AllUnique/app.js
@@ -1,8 +1,16 @@
 function hasUniqueChars(str){
-    // ...
+    var obj = {};
+    for (var i = 0; i < str.length; ++i) {
+      var ch = str[i];
+      if (obj[ch]) return false;
+      obj[ch] = true;
+    }
+    return true;
 }
 
-console.log("  nAa"); // False
-console.log("abcdef"); // True
-console.log("abcdef"); // True
-console.log("aA"); // False
+
+
+console.log(hasUniqueChars("  nAa")); // False
+console.log(hasUniqueChars("abcdef")); // True
+console.log(hasUniqueChars("aA")); // True
+console.log(hasUniqueChars("++-")); // False
