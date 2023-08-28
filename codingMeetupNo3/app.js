ec3c1d173df5655926677b36b9f951ad8d0fa303
gary909 <25279957+gary909@users.noreply.github.com>
2023-08-28 09:33:17 +0200

    code

---
+++ codingMeetupNo3/app.js
@@ -0,0 +1,19 @@
+var list1 = [
+    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+    { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+  ];
+
+  var list2 = [
+    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' } 
+  ];
+
+function isRubyComing(list) {
+    // thank you for checking out my kata :)
+
+
+}
+
+console.log(isRubyComing(list1)); // True
+console.log(isRubyComing(list2)); // False
