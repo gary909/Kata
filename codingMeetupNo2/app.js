df2ba37639328cf8e72f1826de4605ce4d0922c3
gary909 <25279957+gary909@users.noreply.github.com>
2023-08-29 11:29:13 +0200

    code

---
+++ codingMeetupNo2/app.js
@@ -0,0 +1,17 @@
+const list1 = [
+    {
+      firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+    },
+    {
+      firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+    },
+    {
+      firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+    },
+  ];
+
+function greetDevelopers(list) {
+    // thank you for checking out my kata :)
+}
+
+console.log(greetDevelopers(list1)); // greeting: 'Hi Sofia, what do you like the most about Java?'
