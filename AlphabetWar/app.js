ae1e9852e8f743648a2bc0569547d615142997e6
gary909 <25279957+gary909@users.noreply.github.com>
2023-09-01 12:04:06 +0200

    code

---
+++ AlphabetWar/app.js
@@ -0,0 +1,9 @@
+function alphabetWar(fight)
+{
+   return "Let's fight again!";
+}
+
+console.log(alphabetWar("z")); // "Right side wins!" 
+console.log(alphabetWar("zdqmwpbs")); // "Let's fight again!"
+console.log(alphabetWar("zzzzs")); // "Right side wins!" 
+console.log(alphabetWar("wwwwww")); // "Left side wins!" 
