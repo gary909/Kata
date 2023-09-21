bc6f02fbb8e66b2175e2f5931d1693d594302c56
gary909 <25279957+gary909@users.noreply.github.com>
2023-09-21 11:27:57 +0200

    code working

--- DeodorantEvap/app.js
+++ DeodorantEvap/app.js
@@ -1,8 +1,13 @@
 // content in ml - evap % per day - useful in %
 
 function evaporator(content, evap_per_day, threshold){ 
+    let count = 0;
     content = 100;
-    return content;
+    while(content >= threshold){
+        content = content - content * evap_per_day / 100;
+        count = count + 1;
+    }
+    return count;
 }
 
 console.log(evaporator(10,10,10)); // 22
