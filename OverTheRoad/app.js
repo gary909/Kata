551a18ba9926e84b7d5c75a0b18ee130b46687b4
gary909 <25279957+gary909@users.noreply.github.com>
2023-09-22 17:28:53 +0200

    code working

--- OverTheRoad/app.js
+++ OverTheRoad/app.js
@@ -1,8 +1,8 @@
 function overTheRoad(address, n){
-    //code here
+    return (n + n) - address + 1;
 }
 
-console.log(overTheRoad(1,3)); // 4 
+console.log(overTheRoad(1,3)); // 6 
 console.log(overTheRoad(2,3)); // 5
 console.log(overTheRoad(3,5)); // 8
 console.log(overTheRoad(7,11)); // 16
