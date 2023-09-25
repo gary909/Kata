b9b186fab0e77f31c0476ed6bea0e2f6b12a5556
gary909 <25279957+gary909@users.noreply.github.com>
2023-09-25 10:45:22 +0200

    code

---
+++ SumOfCubes/app.js
@@ -0,0 +1,10 @@
+function sumCubes(n){
+    // ...
+}
+
+console.log(sumCubes(1)); // 1
+console.log(sumCubes(2)); // 9
+console.log(sumCubes(3)); // 36
+console.log(sumCubes(4)); // 100
+console.log(sumCubes(10)); // 3025
+console.log(sumCubes(123)); // 58155876
