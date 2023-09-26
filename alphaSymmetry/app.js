943fa823fcaa32f23c5d012f98a195150b0341da
gary909 <25279957+gary909@users.noreply.github.com>
2023-09-26 23:05:44 +0200

    code

---
+++ alphaSymmetry/app.js
@@ -0,0 +1,8 @@
+function solve(arr){  
+    //code
+};
+
+console.log(solve(["abode","ABc","xyzD"])); // [4,3,1]
+console.log(solve(["abide","ABc","xyz"])); // [4,3,0]
+console.log(solve(["IAMDEFANDJKL","thedefgh","xyzDEFghijabc"])); // [6,5,7]
+console.log(solve(["encode","abc","xyzD","ABmD"])); // [1, 3, 1, 3]
