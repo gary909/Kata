979e1573a19a1346c2772fed65b495bbb21dc0c7
gary909 <25279957+gary909@users.noreply.github.com>
2023-09-28 10:40:07 +0200

    code working

--- minSumArray/app.js
+++ minSumArray/app.js
@@ -1,5 +1,38 @@
 function minSum(arr) {
     // your code here
+    let total = 0;
+
+    // duplicate array to avoid mutations
+    let incArr = arr.slice();
+
+    // sort array in num order
+    incArr = incArr.sort(function(a, b) {
+        return a - b;
+    });
+
+    // Halve arr length
+    incArr = incArr.splice(0, Math.floor(incArr.length / 2));
+
+
+
+    // duplicate array to avoid mutations
+    let decArr = arr.slice();
+
+    // sort array in dec order
+    decArr = arr.sort(function(a, b) {
+        return b - a;
+    });  
+
+    // Halve arr length
+    decArr = decArr.splice(0, Math.floor(decArr.length / 2));
+
+
+    // Mulitply results
+    for(let i = 0; i < decArr.length; i++){
+        total = total + (decArr[i] * incArr[i])
+    }
+
+    return total;
 }
 
 console.log(minSum([5,4,2,3])); // 22
