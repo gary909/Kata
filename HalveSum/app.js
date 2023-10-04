dbb4a733245f92f5d34eb56d36994ec29b8113ae
gary909 <25279957+gary909@users.noreply.github.com>
2023-10-04 11:41:27 +0200

    code working

--- HalveSum/app.js
+++ HalveSum/app.js
@@ -1,6 +1,11 @@
 function halvingSum(n) {
-    
+    let sum = 0;
+    for (let i = n; i > 0; i = Math.floor(i / 2)) {
+      sum = sum + i;
+    }
+    return sum;
 }
+
 
 console.log(halvingSum(25)); // 47
 console.log(halvingSum(127)); // 247
