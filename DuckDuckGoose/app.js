dc9ad99f8cc631187f9e276522ecf6ba670b2242
gary909 <25279957+gary909@users.noreply.github.com>
2023-10-05 14:04:04 +0200

    code

---
+++ DuckDuckGoose/app.js
@@ -0,0 +1,14 @@
+function duckDuckGoose(players, goose) {
+    // ...
+}
+
+console.log(duckDuckGoose(players, 1)); // "a"
+console.log(duckDuckGoose(players, 3)); // "c"
+console.log(duckDuckGoose(players, 10)); // "z"
+console.log(duckDuckGoose(players, 20)); // "z"
+console.log(duckDuckGoose(players, 30)); // "z"
+console.log(duckDuckGoose(players, 18)); // "g"
+console.log(duckDuckGoose(players, 28)); // "g"
+console.log(duckDuckGoose(players, 12)); // "b"
+console.log(duckDuckGoose(players, 2)); // "b"
+console.log(duckDuckGoose(players, 7)); // "f"
