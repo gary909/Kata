bd552714b35fcde2bc0f64182d5b3ff16dd0bc8b
gary909 <25279957+gary909@users.noreply.github.com>
2023-10-10 13:07:17 +0200

    code

---
+++ maxDiffEasy/app.js
@@ -0,0 +1,8 @@
+function maxDiff(list) {
+    return 0;
+};
+
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 6])); // 6
+console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6])); // 11
+console.log(maxDiff([16])); // 0
+console.log(maxDiff([0])); // 0
