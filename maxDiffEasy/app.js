04dd940d1efa2d369681954d8156e0d81872c3a4
gary909 <25279957+gary909@users.noreply.github.com>
2023-10-11 17:04:33 +0200

    code working

--- maxDiffEasy/app.js
+++ maxDiffEasy/app.js
@@ -1,5 +1,20 @@
 function maxDiff(list) {
-    return 0;
+    
+    let topNum = list[0];
+    let lowNum = list[0];
+
+    if(list.length == 0 || list.length == 1){
+        return 0;
+    }
+
+    for(let i = 0; i < list.length;i++){
+        if(list[i] > topNum){
+            topNum = list[i]
+        } else if (list[i] < lowNum){
+            lowNum = list[i]
+        }
+    }
+    return topNum - lowNum;
 };
 
 console.log(maxDiff([0, 1, 2, 3, 4, 5, 6])); // 6
