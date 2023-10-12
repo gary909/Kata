ef77f435ced562da701e7dce67516b048d1b7b13
gary909 <25279957+gary909@users.noreply.github.com>
2023-10-12 10:21:18 +0200

    code

---
+++ boiledEggs/app.js
@@ -0,0 +1,8 @@
+function cookingTime(eggs) {
+    // TODO
+}
+
+console.log(cookingTime(0)); // '0'
+console.log(cookingTime(5)); // '5'
+console.log(cookingTime(10)); // '10'
+console.log(cookingTime(8)); // '10'
