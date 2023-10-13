975ece0c3464a8bb20d2d6214d2e79fb25247191
gary909 <25279957+gary909@users.noreply.github.com>
2023-10-13 10:45:40 +0200

    redme images

--- boiledEggs/app.js
+++ boiledEggs/app.js
@@ -5,4 +5,4 @@ function cookingTime(eggs) {
 console.log(cookingTime(0)); // '0'
 console.log(cookingTime(5)); // '5'
 console.log(cookingTime(10)); // '10'
-console.log(cookingTime(8)); // '10'
+console.log(cookingTime(8)); // '5'
