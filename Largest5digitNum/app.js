ac43016f37ebc65030ff34ec75afe110e81e7701
gary909 <25279957+gary909@users.noreply.github.com>
2023-10-20 13:22:32 +0200

    code working

--- Largest5digitNum/app.js
+++ Largest5digitNum/app.js
@@ -1,5 +1,12 @@
 function solution(digits){
-  
+  let myNum = 0;
+  for(let i = 0; i < digits.length; i++){
+    let number = digits.substr(i, 5); 
+    if(Number(number) >= myNum){
+        myNum = Number(number);
+        }
+    }
+    return myNum
 }
 
 console.log(solution('1234567898765')); // 98765    
