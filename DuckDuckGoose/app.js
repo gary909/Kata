2b13ba64c87df274a86244f2d891d077ef10c3a5
gary909 <25279957+gary909@users.noreply.github.com>
2023-10-23 09:09:44 +0200

    code

--- DuckDuckGoose/app.js
+++ DuckDuckGoose/app.js
@@ -1,14 +1,19 @@
 function duckDuckGoose(players, goose) {
     // ...
+    let  count = 0;
+    for(let i = 0; i < goose; i++){
+        
+    }
+    return players[0];
 }
 
-console.log(duckDuckGoose(players, 1)); // "a"
-console.log(duckDuckGoose(players, 3)); // "c"
-console.log(duckDuckGoose(players, 10)); // "z"
-console.log(duckDuckGoose(players, 20)); // "z"
-console.log(duckDuckGoose(players, 30)); // "z"
-console.log(duckDuckGoose(players, 18)); // "g"
-console.log(duckDuckGoose(players, 28)); // "g"
-console.log(duckDuckGoose(players, 12)); // "b"
-console.log(duckDuckGoose(players, 2)); // "b"
-console.log(duckDuckGoose(players, 7)); // "f"
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 1)); // "a"
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 3)); // "c"
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 10)); // "z"
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 20)); // "z"
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 30)); // "z"
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 18)); // "g"
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 28)); // "g"
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 12)); // "b"
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 2)); // "b"
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 7)); // "f"
