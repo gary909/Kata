e3bce9d35090464fbef3c1a88cdcc44623fe08cd
gary909 <25279957+gary909@users.noreply.github.com>
2023-10-24 12:20:39 +0200

    code

---
+++ SumOfNum0toN/app.js
@@ -0,0 +1,13 @@
+var SequenceSum = (function() {
+    function SequenceSum() {}
+  
+    SequenceSum.showSequence = function(count) {
+      // for
+        //while
+    };
+  
+    return SequenceSum;
+  
+  })();
+
+  console.log(showSequence(6)); // "0+1+2+3+4+5+6 = 21"
