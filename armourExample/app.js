6eac94785e6833c6f165b6919f3ab93e40888a86
gary909 <25279957+gary909@users.noreply.github.com>
2023-11-06 11:20:24 +0100

    init

---
+++ armourExample/app.js
@@ -0,0 +1,7 @@
+function doomArmour(health, armour, hit){
+
+}
+
+console.log(doomArmour(100, 20, 10)) // Return Life 100 Armour 10
+console.log(doomArmour(100, 20, 30)) // Return Life 90 Armour 0
+console.log(doomArmour(100, 20, 72)) // Return Life 48 Armour 0
