d701f204acd4b063f767ffecccd1ccdac6ac52a6
gary909 <25279957+gary909@users.noreply.github.com>
2023-11-07 10:35:18 +0100

    code changes

--- armourExample/app.js
+++ armourExample/app.js
@@ -8,6 +8,27 @@ function doomArmour(health, armour, hit){
     return 'Health: ' + health + ' Armour: ' + initDamage;
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    // Wait for the DOM to be fully loaded before adding event listeners.
+
+    var calculateButton = document.getElementById("calculateButton");
+    var number1Input = document.getElementById("number1");
+    var number2Input = document.getElementById("number2");
+    var resultParagraph = document.getElementById("result");
+
+    calculateButton.addEventListener("click", function () {
+        var number1 = parseFloat(number1Input.value);
+        var number2 = parseFloat(number2Input.value);
+
+        if (!isNaN(number1) && !isNaN(number2)) {
+            var sum = number1 + number2;
+            resultParagraph.textContent = "Sum: " + sum;
+        } else {
+            resultParagraph.textContent = "Please enter valid numbers.";
+        }
+    });
+});
+
 console.log(doomArmour(100, 20, 10)) // Return Life 100 Armour 10
 console.log(doomArmour(100, 20, 30)) // Return Life 90 Armour 0
 console.log(doomArmour(100, 20, 72)) // Return Life 48 Armour 0
