095b4e9bae240ed62f18785b873f03aa595f7ed6
gary909 <25279957+gary909@users.noreply.github.com>
2023-11-13 11:28:41 +0100

    code working

--- removeDuplicates/app.js
+++ removeDuplicates/app.js
@@ -1,6 +1,24 @@
 function solve(arr) {
-    return [];
-}
+    const uniqueMap = {};
+
+    // Iterate through the array in reverse order
+    for (let i = arr.length - 1; i >= 0; i--) {
+      const currentElement = arr[i];
+  
+      // If the element is not in the map, add it with its index
+      if (!(currentElement in uniqueMap)) {
+        uniqueMap[currentElement] = i;
+      }
+    }
+  
+    // Extract the values (indices) from the map and sort them
+    const uniqueIndices = Object.values(uniqueMap).sort((a, b) => a - b);
+  
+    // Create a new array with the unique elements in the original order
+    const uniqueArray = uniqueIndices.map((index) => arr[index]);
+  
+    return uniqueArray;
+  }
 
 console.log(solve([3,4,4,3,6,3])) // [4,6,3]
 console.log(solve([1,2,1,2,1,2,3])) // [1,2,3]
