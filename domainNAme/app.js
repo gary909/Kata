34ba896bf48e6ea2455a697702df2cef1f77ebcf
gary909 <25279957+gary909@users.noreply.github.com>
2024-11-29 11:15:41 +0100

    mostly working x2

--- domainNAme/app.js
+++ domainNAme/app.js
@@ -1,9 +1,17 @@
 function domainName(url){
-    const words = url.split('://');
-    const back = words[1];
-    const back2 = back;
-    const back3 = back2.split('.');
-    console.log(back3[0]);
+    if(url.includes("://")){
+        const words = url.split('://');
+        const back = words[1];
+        const back2 = back;
+        const back3 = back2.split('.');
+        console.log(back3[0]);
+    } else {
+        const words = url.split('www.');
+        const back = words[1];
+        const back2 = back;
+        const back3 = back2.split('.');
+        console.log(back3[0]);
+    }
   }
 
 console.log(domainName("http://google.com")); // google
